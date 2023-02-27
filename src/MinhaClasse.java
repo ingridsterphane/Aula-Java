b51aabb15e3366bc1cd2eed0842ac08371f39866
Ingrid <ingridsterphane@hotmail.com>
2023-02-27 17:53:43 -0300

    Update

--- src/MinhaClasse.java
+++ src/MinhaClasse.java
@@ -1,6 +1,9 @@
 public class MinhaClasse {
     public static void main(String [] args) {
-
+        String MeuNome = "Ingrid";
+        int anoformacao = 2020;
+        boolean verdadeira = false;
+        anoformacao = 2021;
        
     }
     
