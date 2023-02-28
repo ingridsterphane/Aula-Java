9a850772285dbdcefb2c7d30eb731eeda6c79e08
ingridsterphane <ingridsterphane@hotmail.com>
2023-02-28 10:30:01 -0300

    UPDATE

--- TiposVariaveis.java
+++ TiposVariaveis.java
@@ -1,5 +1,17 @@
 public class TiposVariaveis {
     public static void main (String[] args) throws Exception {
         double salarioMinimo = 2.5;
+
+        short numeroCurto =1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 1;
+
+        final double VALOR_DE_PI = 3.14;
+        //VALOR_DE_PI = 10; 
+        //DEU ERRADO PQ FOI COLOCADO O "FINAL" QUE FIXOU O VALOR DE 3.14 COMO CONSTANTE
+        //ENTÃO NÃO PODE SER ALTERADO PARA 10
+
     }
 }
